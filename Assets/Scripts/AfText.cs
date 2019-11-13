aa6ad72549be36f2dde1e79fdfdd49f51b0311ce
residentrayman <57415449+residentrayman@users.noreply.github.com>
2019-11-13 17:36:58 +0100

    V2 alpha UI
    Hna 9adito
    zadt l dakchi li kint dart f projet dyali

--- Assets/Scripts/AfText.cs
+++ Assets/Scripts/AfText.cs
@@ -3,30 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AfText : MonoBehaviour {
+public class AfText : MonoBehaviour
+{
+     //new
     //public GameObject Afficher;
-    public Text Qaaf = null;
-    public Text Qaaf1 = null;
-    public Text Qaaf2 = null;
-    public Text Qaaf3 = null;
-    public Text Qaaf4 = null;
-    public Text Qaaf5 = null;
-//b3aba3
+    public Text UserName = null;
+    public Text Time = null;
+    public Text QAAficher = null;
+    public Text UserShortN = null;
+    public GameObject Post = null;
+    //public Camera cam = null;
 
 
     // Use this for initialization
-    void Start () {
-        Qaaf.text = "Hellow";
-        //Qaaf1.text = "Azer"; 
+    void Start()
+    {
+        UserName.text = "tippaRayman";
+        Time.text = "14:36";
+        QAAficher.text = "Posting . . . . . ! !!";
+        UserShortN.text = "TR";
+
+        Vector2 localpoint = new Vector2(232f, 270);
+        Post.gameObject.transform.position = localpoint * 1.37f;
+
+        StartCoroutine(Wait(3.0f));
+
+
     }
 
-    public void changText(string newText)
-    {
-        float temp = float.Parse(newText);
-    }
+    //public void changText(string newText)
+    //{
+    //    float temp = float.Parse(newText);
+    //}
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+        
+
+    }
+
+    IEnumerator Wait(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Post.gameObject.SetActive(true);
+    }
+
+    public void ClickB()
+    {
+        QAAficher.text = "";
+        Debug.Log("oy");
+    }
 }
