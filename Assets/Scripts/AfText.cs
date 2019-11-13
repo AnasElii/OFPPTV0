4ca0bfb3b82d711a639f47a19ad7fd32a681cb01
AninossII <57045844+AninossII@users.noreply.github.com>
2019-11-13 01:05:40 +0100

    Text Problem Fix

---
+++ Assets/Scripts/AfText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AfText : MonoBehaviour {
+    //public GameObject Afficher;
+    public Text Qaaf = null;
+    public Text Qaaf1 = null;
+    public Text Qaaf2 = null;
+    public Text Qaaf3 = null;
+    public Text Qaaf4 = null;
+    public Text Qaaf5 = null;
+
+
+    // Use this for initialization
+    void Start () {
+        Qaaf.text = "Hellow";
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Post.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Text;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+    private string Nom;
+    private string Email;
+    private string Comment;
+    private string QA;
+
+    public GameObject Afficher;
+    public GUIText  Qaaf = null;
+
+    void awaike()
+    {
+        
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+        Qaaf.text = "Hellow";
+
+        Afficher = GameObject.Find("QAAfficher");
+
+        Qaaf = Afficher.GetComponent<GUIText>();
+
+        Qaaf.text = "Text 01";
+
+        Afficher.GetComponent<GUIText>().text = Qaaf.text;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+
+    }
+}
